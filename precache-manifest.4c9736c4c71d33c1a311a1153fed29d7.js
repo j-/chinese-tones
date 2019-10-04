9c4caae5e365d695a928092fdda5819175d0d594
j- <j@skeoh.com>
2019-10-04 14:54:02 +1000

    Updates

---
+++ precache-manifest.4c9736c4c71d33c1a311a1153fed29d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5cb79570d99dc0f1ebcf93af6273961",
+    "url": "/chinese-tones/index.html"
+  },
+  {
+    "revision": "0c3e55e9bb5b736d6433",
+    "url": "/chinese-tones/static/css/2.545c6387.chunk.css"
+  },
+  {
+    "revision": "0c3e55e9bb5b736d6433",
+    "url": "/chinese-tones/static/js/2.7438ea16.chunk.js"
+  },
+  {
+    "revision": "c39b47c5210071ae0fc1",
+    "url": "/chinese-tones/static/js/main.18fe6f3c.chunk.js"
+  },
+  {
+    "revision": "5489704b106b3817d14e",
+    "url": "/chinese-tones/static/js/runtime-main.427ea694.js"
+  }
+]);
