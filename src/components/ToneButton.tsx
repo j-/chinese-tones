56bc86c77de134e13bc469c58b72734b2440c5a8
j- <j@skeoh.com>
2019-10-22 16:57:38 +1100

    Add colors to tones

--- src/components/ToneButton.tsx
+++ src/components/ToneButton.tsx
@@ -14,25 +14,25 @@ const ToneButton: React.FC<Props> = (props) => (
 export default ToneButton;
 
 export const ToneButton1: React.FC<Props> = ({ currentVowel, ...props }) => (
-  <ToneButton value={1} {...props}>
+  <ToneButton value={1} style={{ color: 'red' }} {...props}>
     <big>{(currentVowel || DOTTED_CIRCLE) + MACRON}</big><br /><small>1</small>
   </ToneButton>
 );
 
 export const ToneButton2: React.FC<Props> = ({ currentVowel, ...props }) => (
-  <ToneButton value={2} {...props}>
+  <ToneButton value={2} style={{ color: 'orange' }} {...props}>
     <big>{(currentVowel || DOTTED_CIRCLE) + ACUTE}</big><br /><small>2</small>
   </ToneButton>
 );
 
 export const ToneButton3: React.FC<Props> = ({ currentVowel, ...props }) => (
-  <ToneButton value={3} {...props}>
+  <ToneButton value={3} style={{ color: 'green' }} {...props}>
     <big>{(currentVowel || DOTTED_CIRCLE) + CARON}</big><br /><small>3</small>
   </ToneButton>
 );
 
 export const ToneButton4: React.FC<Props> = ({ currentVowel, ...props }) => (
-  <ToneButton value={4} {...props}>
+  <ToneButton value={4} style={{ color: 'blue' }} {...props}>
     <big>{(currentVowel || DOTTED_CIRCLE) + GRAVE}</big><br /><small>4</small>
   </ToneButton>
 );
