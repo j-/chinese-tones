c35441924b11834f3d8112006947796e030230c1
j- <j@skeoh.com>
2020-09-14 23:44:16 +1000

    Align to brand

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,7 +4,7 @@ import Convert from './Convert';
 const App: React.FC = () => (
   <div className="App container my-5">
     <a href="https://skeoh.com/" className="text-secondary">&larr; skeoh.com</a>
-    <h1>Chinese Tones</h1>
+    <h1>Chinese tones</h1>
     <section className="my-5">
       <Convert />
     </section>
